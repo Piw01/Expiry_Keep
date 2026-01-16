b6b8508fbdfb18e3bf7b9d02824cb92814aa9525
Piw01 <amfibloods@gmail.com>
2026-01-16 11:28:26 +0700

    commit

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -6,8 +6,6 @@ import 'package:note_app/screens/product/product_detail_screen.dart';
 import '../../../providers/providers.dart';
 import '../../../models/product_model.dart';
 import '../../../services/auth_service.dart';
-import '../../product/add_product_screen.dart';
-import '../../product/product_detail_screen.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
